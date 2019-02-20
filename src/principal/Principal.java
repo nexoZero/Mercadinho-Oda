b6ccd5ffe4acfcf0b808cf1a60e6541b64f43607
nexoZero <sigma.leonnard@gmail.com>
2019-02-21 05:53:28 +0700

    Mercadinho Oda criad

---
+++ src/principal/Principal.java
@@ -0,0 +1,16 @@
+package principal;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ola mundo, eu nasce para consquistar-lo");
+    }
+    
+}
